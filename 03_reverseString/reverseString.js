97cc496543f1c45d39e1dff3625c933ee5bdadac
victor <medina.victor.m@protonmail.com>
2022-01-05 08:29:35 +0100

    Solution to the exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
 const reverseString = function(str) {
-
+  let strToReturn = "";
+  for(let i = str.length-1; i >= 0; i--) {
+    strToReturn += str.charAt(i);
+  }
+  return strToReturn;
 };
 
 // Do not edit below this line
