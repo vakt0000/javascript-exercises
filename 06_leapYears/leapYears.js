0b31b7deb5fee9d403e52d81af8c9a9273dc0f4d
victor <medina.victor.m@protonmail.com>
2022-01-05 10:33:53 +0100

    Solution for the exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  if(year%4===0) {
+    year = year/4;
+    if(year%100===0) return true;
+    else if(year%25===0) return false;
+    else return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
