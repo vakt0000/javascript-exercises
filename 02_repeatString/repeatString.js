f8fdbfb80d217bb81ee2ed0b9760c0d86b21daf3
victor <medina.victor.m@protonmail.com>
2022-01-05 08:19:29 +0100

    Solution to the proposed exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+  let strToReturn = "";
+  if(n < 0) {
+    return "ERROR";
+  }
+  for(let i = 0; i < n; i++) {
+        strToReturn += str;
+  }
+  return strToReturn;
 };
 
 // Do not edit below this line
