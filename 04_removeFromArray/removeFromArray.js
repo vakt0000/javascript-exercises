a9a761302847bca3767c74ccbd706790bb012d9e
victor <medina.victor.m@protonmail.com>
2022-01-05 09:18:19 +0100

    Solution to the proposed exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...theArgs) {
+  let arrayElements = Array.from(theArgs);
+  for(let k=0; k<arrayElements.length; k++) {
+    let offset = 0;
+    for(let i=0; i<array.length; i++) {
+      if(array[i]===arrayElements[k]) {
+        array.splice(i+offset, 1);
+        offset--;
+      }
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
