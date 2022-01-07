d701e632176793111e2c38e97463bfda53edbdc0
victor <medina.victor.m@protonmail.com>
2022-01-07 21:12:33 +0100

    Made the code shorter

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,16 +1,14 @@
 const fibonacci = function(n) {
   if(n<=0) return "OOPS";
   if(n<3) return 1;
-  else {
-    let last_item = 2;
-    let precedent_item = 1;
-    while(n > 3) {
-      last_item += precedent_item;
-      precedent_item = last_item-precedent_item;
-      n--;
-    }
-    return last_item;
+  let last_item = 2;
+  let precedent_item = 1;
+  while(n > 3) {
+    last_item += precedent_item;
+    precedent_item = last_item-precedent_item;
+    n--;
   }
+  return last_item;
 }
 
 // Do not edit below this line
