4de53b647a1effc2b5c5a5227177b878c051eaf0
victor <medina.victor.m@protonmail.com>
2022-01-07 21:15:14 +0100

    Revisited the exercise once I learnt Array map

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,9 +1,6 @@
 const getTheTitles = function(array) {
-  for(let i = 0; i < array.length; i++) {
-    array.splice(i, 1, array[i].title);
-  }
-  return array;
-};
+  return array.map(book => book.title);
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
